b9007da9605f44611afd5800b02638926adb999b
arshad-shah <shaharshad57@gmail.com>
2022-01-22 13:59:53 +0000

    added the asmaulhusna audio to the names of allah

--- app/src/main/java/com/arshadshah/nimaz/fragments/NamesFragment.kt
+++ app/src/main/java/com/arshadshah/nimaz/fragments/NamesFragment.kt
@@ -1,11 +1,18 @@
 package com.arshadshah.nimaz.fragments
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
 import com.arshadshah.nimaz.R
 import com.arshadshah.nimaz.helperClasses.CustomAdapter
 import com.arshadshah.nimaz.helperClasses.SubjectData
@@ -21,7 +28,7 @@ class NamesFragment : Fragment()
     {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_names , container , false)
-
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
 
 
         val list : ListView = root.findViewById(R.id.allahNames)
@@ -42,6 +49,47 @@ class NamesFragment : Fragment()
         }
         val customAdapter = CustomAdapter(requireContext() , arrayList)
         list.adapter = customAdapter
+
+
+        //get the play button
+        val playButton: ImageButton = root.findViewById(R.id.namesOfAllahAudio)
+
+        val isAudioPlaying = sharedPreferences.getBoolean("isAudioPlaying" , false)
+        //when play button is clicked
+        playButton.setOnClickListener {
+            with(sharedPreferences.edit()) {
+                putBoolean("isAudioPlaying" , isAudioPlaying)
+                apply()
+            }
+            val expandIn : Animation =
+                AnimationUtils.loadAnimation(requireContext() , R.anim.expand_in)
+            playButton.startAnimation(expandIn)
+            val mediaPlayer = MediaPlayer()
+            if(isAudioPlaying){
+                //start the audio
+                val myUri: Uri = Uri.parse("android.resource://" + context?.packageName + "/" + R.raw.asmaulhusna)
+                mediaPlayer.apply {
+                    setAudioAttributes(
+                        AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+                    )
+                    setDataSource(requireContext(), myUri)
+                    prepare()
+                    start()
+                }
+                //change the button imgae to stop
+                playButton.setImageResource(R.drawable.ic_square)
+            }
+            else{
+                mediaPlayer.pause()
+                mediaPlayer.stop()
+                mediaPlayer.release()
+                //change the button imgae to stop
+                playButton.setImageResource(R.drawable.ic_play)
+            }
+        }
 
         return root
     }
