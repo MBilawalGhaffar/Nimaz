813c55d6465ea928368edf8cc787a255f579874b
Arshad Shah <49516613+arshad-shah@users.noreply.github.com>
2022-04-22 18:08:01 +0100

    added loading state to location init

--- app/src/main/java/com/arshadshah/nimaz/fragments/intro/LocationSelectionFragment.kt
+++ app/src/main/java/com/arshadshah/nimaz/fragments/intro/LocationSelectionFragment.kt
@@ -3,15 +3,23 @@ package com.arshadshah.nimaz.fragments.intro
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.preference.PreferenceManager
 import com.arshadshah.nimaz.R
 import com.arshadshah.nimaz.activities.HomeActivity
+import com.arshadshah.nimaz.helperClasses.fusedLocations.LocationFinderAuto
+import com.arshadshah.nimaz.helperClasses.fusedLocations.PermissionUtils
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class LocationSelectionFragment : Fragment() {
 
@@ -23,30 +31,100 @@ class LocationSelectionFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_location_selection, container, false)
 
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
-
-        val manual: Button = root.findViewById(R.id.manual)
-        val auto: Button = root.findViewById(R.id.auto)
+        val locationTypeValue = sharedPreferences.getBoolean("locationType", true)
+        val locationFinish: Button = root.findViewById(R.id.locationFinish)
         val locationSkip: Button = root.findViewById(R.id.locationSkip)
 
-        manual.setOnClickListener {
-            val navcontroller = requireActivity().findNavController(R.id.fragmentContainerView)
-            navcontroller.navigate(R.id.locationInputFragment)
+        val locationPref: SwitchMaterial = root.findViewById(R.id.locationPref)
+
+        if(!locationPref.isChecked){
             with(sharedPreferences.edit()) {
                 putBoolean("locationType", false)
                 apply()
             }
         }
-        auto.setOnClickListener {
 
+        locationPref.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                with(sharedPreferences.edit()) {
+                    putBoolean("locationType", true)
+                    apply()
+                }
+                locationPref.text = "Automatic"
+                PermissionUtils.askAccessLocationPermission(requireActivity() as AppCompatActivity,12345)
+            }else {
+                with(sharedPreferences.edit()) {
+                    putBoolean("locationType", false)
+                    apply()
+                }
+                locationPref.text = "Manual"
+            }
+        }
+
+
+
+
+
+        val batteryOpt: SwitchMaterial = root.findViewById(R.id.batteryOpt)
+
+        val pm: PowerManager =
+            requireContext().getSystemService(AppCompatActivity.POWER_SERVICE) as PowerManager
+
+        batteryOpt.isChecked = pm.isIgnoringBatteryOptimizations(requireContext().packageName)
+
+        //if batterOpt is true then disable the switch
+        batteryOpt.isEnabled = !batteryOpt.isChecked
+
+
+        batteryOpt.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+                startActivity(intent)
+            }
+        }
+
+        // the Progress container progressContainer, and the mainCard 
+        val progressContainer: CardView = root.findViewById(R.id.progressContainer)
+        val mainCard: CardView = root.findViewById(R.id.mainCard)
+
+        locationFinish.setOnClickListener {
             with(sharedPreferences.edit()) {
-                putBoolean("locationType", true)
+                putBoolean("isFirstInstall", false)
+                putBoolean("navigateToHome", true)
                 putBoolean("channelLock", false)
                 apply()
             }
+            if (locationTypeValue) {
+                LocationFinderAuto().getLocations(requireContext(), 12345)
+            }
 
-            val navcontroller = requireActivity().findNavController(R.id.fragmentContainerView)
-            navcontroller.navigate(R.id.serviceInitFragment)
+            //create a thread to show the progress bar
+            //while the longitude and latitude values are "0.0" hide mainCard and show a progress bar
+            //then when the values are set navigate to HomeActivity
+            //wait for the longitude and latitude values to not be null then continue
+            //show the progress bar and hide the mainCard
+            progressContainer.visibility = View.VISIBLE
+            mainCard.visibility = View.GONE
+            val thread = Thread {
+                while (true) {
+                    if (sharedPreferences.getString("longitude", "0.0") != "0.0" && sharedPreferences.getString(
+                            "latitude",
+                            "0.0"
+                        ) != "0.0"
+                    ) {
+                        //on ui thread
+                        activity?.runOnUiThread {
+                            val intent = Intent(requireContext(), HomeActivity::class.java)
+                            startActivity(intent)
+                            requireActivity().finish()
+                        }
+                        break
+                    }
+                }
+            }
+            thread.start()
         }
+
         //if skipped
         locationSkip.setOnClickListener {
             createDialog()
